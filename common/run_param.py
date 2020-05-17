30373d5122b03459cc68960159be5e78190b09c5
simonlisiyu <simonlisiyu@didichuxing.com>
2020-05-17 18:17:06 +0800

    update

---
+++ common/run_param.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import sys
+
+print '参数个数为:', len(sys.argv), '个参数。'
+print '参数列表:', str(sys.argv)
+print '参数列表:', str(sys.argv[0])
+print '参数列表:', str(sys.argv[1])
+print '参数列表:', str(sys.argv[2])
+
+# python test1.py 123 222
